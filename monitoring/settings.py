dd132294ab3dda6a61c1cb3d74055920bc50f3f3
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-08-21 21:22:07 -0500

    Talleres 3.1 y 3.2

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": 'dbtaller_3',
+        "USER": "efblanco",
+        "PASSWORD": "blanc0DB1",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
